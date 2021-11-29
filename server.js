7ebfe20cecedf852bf554a4d977c3f9275968bb9
Logner <=>
2021-11-29 16:08:48 -0500

    Added zookeeper functionality

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require ('express');
-const fs = require('fs');
-const path = require('path');
-const {animals} = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiroutes/animalRoutes.js')
-const htmlRoutes = require('./routes/htmlroutes/index.js')
+const apiRoutes = require('./routes/apiroutes')
+const htmlRoutes = require('./routes/htmlroutes')
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
